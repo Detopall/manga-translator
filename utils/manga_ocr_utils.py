8b6bee0148805329662a46af60773abbc6f2857e
Detopall <denis.topallaj13@gmail.com>
2025-02-24 20:47:28 +0100

    fixed issue with rerunning mocr in function, way faster now

--- utils/manga_ocr_utils.py
+++ utils/manga_ocr_utils.py
@@ -4,15 +4,14 @@ This module is used to extract text from images using manga_ocr.
 
 from manga_ocr import MangaOcr
 
+mocr = MangaOcr()
 
 def get_text_from_image(image):
 	"""
 	Extract text from images using manga_ocr.
 	"""
-	print(image)
 
 	try:
-		mocr = MangaOcr()
 		result = mocr(image)
 		return result
 	except Exception as e:
